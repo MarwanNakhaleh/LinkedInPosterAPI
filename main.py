b337006153a9abd22c11d9dffdd0831207801cf5
Marwan Nakhaleh <marwan@Marwans-MacBook-Air.local>
2023-03-16 12:05:18 -0400

    checking if given category from API exists

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import json
+import uuid
 from base64 import b64decode
 
 from helpers import dynamodb_table
@@ -9,23 +10,60 @@ logging.basicConfig(format="%(asctime)s: %(levelname)s: %(message)s")
 log = logging.getLogger("LinkedInPosterAPI")
 log.setLevel(logging.INFO)
 
+def get_all_categories(table):
+    try:
+        response = table.scan()
+        categories = [x["category"] for x in response["Items"]]
+        log.info("found categories " + categories)
+        return categories
+    except Exception as e:
+        log.error("Unable to get categories from table: " + e)
+        return []
+    
+
 def lambda_handler(event, context):
     try:
         log.info(json.dumps(event))
         event_body = json.loads(b64decode(event["body"]))
 
         posts_table = dynamodb_table(os.environ["POST_TABLE"])
+        categories_table = dynamodb_table(os.environ["CATEGORY_TABLE"])
+        categories = get_all_categories(categories_table)
         
-        return {
-            "headers": {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "*",
-                "Access-Control-Allow-Credentials": True,
-                "Content-Type": "application/json"
-            },
-            "statusCode": 201,
-            "body": "dookie"
-        }
+        if event_body["category"] in categories:
+            response = posts_table.put_item(
+                Item={
+                    'content': event_body["content"],
+                    'hasBeenPosted': "false",
+                    'id': uuid.uuid4(),
+                    'links': event_body["links"],
+                    'category': event_body["category"]
+                }
+            )
+            
+            return {
+                "headers": {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "*",
+                    "Access-Control-Allow-Credentials": True,
+                    "Content-Type": "application/json"
+                },
+                "statusCode": 201,
+                "body": response
+            }
+        else:
+            return {
+                "headers": {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "*",
+                    "Access-Control-Allow-Credentials": True,
+                    "Content-Type": "application/json"
+                },
+                "statusCode": 400,
+                "body": json.dumps({
+                    "error": "Choose an existing category from the following: " + categories
+                })
+            }
     except Exception as e:
         log.error(str(e))
         return {
