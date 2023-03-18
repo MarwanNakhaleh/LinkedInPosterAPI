e18f01358232209a155f52e22077d914abc01670
Marwan Nakhaleh <marwan@Marwans-MacBook-Air.local>
2023-03-17 20:30:58 -0400

    authorizer works! now to deploy it through CircleCI

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # LinkedIn Post API
 This API will allow me to send requests to add potential LinkedIn posts to my database. In the future, I might make a front-end that communicates with this API.
+
+### Authorization
+Authorizer is based almost entirely on the [Serverless Demo by @mukulmantosh](https://github.com/mukulmantosh/ServerlessDemo).
---
+++ cleanup.sh
@@ -0,0 +1,13 @@
+rm -rf authorizer/cffi*/
+rm -rf authorizer/cryptography*/
+rm -rf authorizer/deprecated*/
+rm -rf authorizer/Deprecated*/
+rm -rf authorizer/jwcrypto*/
+rm -rf authorizer/pycparser*/
+rm -rf authorizer/python_jwt*/
+rm -rf authorizer/share/
+rm -rf authorizer/wrapt*/
+rm -rf authorizer/PyJWT*/
+rm -rf authorizer/jwt/
+rm authorizer/*.so
+rm -rf .aws-sam/
